0000ac2b886f2008a77c4bededb1d05cd0020bae
nan0silver <silvernh1220@gmail.com>
2025-07-23 14:37:28 +0900

    feat: first init

---
+++ src/test/java/org/example/chingoohaja/ChingooHajaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chingoohaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChingooHajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
