31c234132e37e7be44033fb19cb21671a5c66855
nan0silver <silvernh1220@gmail.com>
2025-07-24 09:24:53 +0900

    chore: main class 패키지 경로 수정

--- src/main/java/com/ldsilver/chingoohaja/ChingooHajaApplication.java
+++ src/main/java/com/ldsilver/chingoohaja/ChingooHajaApplication.java
@@ -1,4 +1,4 @@
-package org.example.chingoohaja;
+package com.ldsilver.chingoohaja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
