7ea7ef92bacacbf1275976ca2a34414fc826b4c2
nan0silver <silvernh1220@gmail.com>
2025-07-26 12:25:42 +0900

    feat: call enum 클래스 생성

---
+++ src/main/java/com/ldsilver/chingoohaja/domain/call/enums/CallStatus.java
@@ -0,0 +1,5 @@
+package com.ldsilver.chingoohaja.domain.call.enums;
+
+public enum CallStatus {
+    READY, IN_PROGRESS, COMPLETED, CANCELLED, FAILED
+}
---
+++ src/main/java/com/ldsilver/chingoohaja/domain/call/enums/CallType.java
@@ -0,0 +1,5 @@
+package com.ldsilver.chingoohaja.domain.call.enums;
+
+public enum CallType {
+    RANDOM_MATCH, GUARDIAN_CALL, FRIEND_CALL
+}
