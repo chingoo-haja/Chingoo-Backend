10e880881deed53da7ede3e8595eacb7ebf9581c
nan0silver <silvernh1220@gmail.com>
2025-07-31 14:21:54 +0900

    feat: Custom UserDetails 구현

---
+++ src/main/java/com/ldsilver/chingoohaja/domain/user/CustomUserDetails.java
@@ -0,0 +1,46 @@
+package com.ldsilver.chingoohaja.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String authority = "ROLE_" + user.getUserType().name();
+        return Collections.singletonList(new SimpleGrantedAuthority(authority));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    public String getUserType() {
+        return user.getUserType().name();
+    }
+}
