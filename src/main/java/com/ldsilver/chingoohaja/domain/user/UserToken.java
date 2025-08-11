60dbfdcb0ebea47e9d785e7db64489e81ccaa764
nan0silver <silvernh1220@gmail.com>
2025-08-11 16:51:47 +0900

    feat: new USer DB insert관련 에러 수정 - JPA Auditing 활성화 - UserToken 엔티티 refreshToken 길이 1000으로 늘림

--- src/main/java/com/ldsilver/chingoohaja/domain/user/UserToken.java
+++ src/main/java/com/ldsilver/chingoohaja/domain/user/UserToken.java
@@ -22,7 +22,7 @@ public class UserToken extends BaseEntity {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 1000)
     private String refreshToken;
 
     @Column(nullable = false)
