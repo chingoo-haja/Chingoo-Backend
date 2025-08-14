5ec52f6b4f9b98666dfc8649cdce67180a5f153f
nan0silver <silvernh1220@gmail.com>
2025-08-14 15:27:35 +0900

    feat: 프로필 이미지 수정 엔드포인트 구현

--- src/main/java/com/ldsilver/chingoohaja/controller/UserController.java
+++ src/main/java/com/ldsilver/chingoohaja/controller/UserController.java
@@ -2,7 +2,9 @@ package com.ldsilver.chingoohaja.controller;
 
 import com.ldsilver.chingoohaja.domain.user.CustomUserDetails;
 import com.ldsilver.chingoohaja.dto.common.ApiResponse;
+import com.ldsilver.chingoohaja.dto.user.request.ProfileImageUploadRequest;
 import com.ldsilver.chingoohaja.dto.user.request.ProfileUpdateRequest;
+import com.ldsilver.chingoohaja.dto.user.response.ProfileImageUploadResponse;
 import com.ldsilver.chingoohaja.dto.user.response.ProfileResponse;
 import com.ldsilver.chingoohaja.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,8 +13,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -54,5 +58,23 @@ public class UserController {
         );
 
         return ApiResponse.ok("프로필 수정 성공", updateProfile);
+    }
+
+    @Operation(
+            summary = "프로필 이미지 업로드",
+            description = "현재 로그인한 사용자의 프로필 이미지를 업로드합니다." +
+                    "지원 형식: JPEG, PNG, WebP / 최대 크기: 5MB"
+    )
+    @PostMapping(value = "/profile/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ApiResponse<ProfileImageUploadResponse> uploadProfileImage(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestParam("image")MultipartFile image) {
+        log.debug("프로필 이미지 업로드 요청 - userId: {}", userDetails.getUserId());
+
+        ProfileImageUploadRequest request = ProfileImageUploadRequest.from(image);
+        ProfileImageUploadResponse response = userService.updateProfileImage(
+                userDetails.getUserId(), request
+        );
+        return ApiResponse.ok("프로필 이미지 업로드 성공", response);
     }
 }
