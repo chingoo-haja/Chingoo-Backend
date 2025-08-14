b6049700214667060375f839443830dcba6dd60c
nan0silver <silvernh1220@gmail.com>
2025-08-14 15:16:24 +0900

    feat: 이미지 파일 관련 에러코드 추가

--- src/main/java/com/ldsilver/chingoohaja/common/exception/ErrorCode.java
+++ src/main/java/com/ldsilver/chingoohaja/common/exception/ErrorCode.java
@@ -47,6 +47,12 @@ public enum ErrorCode {
     INVALID_USER_TYPE(HttpStatus.BAD_REQUEST, "U004", "유효하지 않은 사용자 타입입니다."),
     PROFILE_NOT_COMPLETED(HttpStatus.BAD_REQUEST, "U005", "프로필 정보가 완성되지 않았습니다."),
     USER_CREATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "U006", "사용자 생성에 실패했습니다."),
+    IMAGE_SAVE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "U007", "이미지 저장 중 오류가 발생했습니다."),
+    IMAGE_FILE_EMPTY(HttpStatus.INTERNAL_SERVER_ERROR, "U008", "이미지 파일이 비어있습니다."),
+    IMAGE_FILE_OVERSIZED(HttpStatus.INTERNAL_SERVER_ERROR, "U009", "이미지 파일 크기는 5MB를 초과할 수 없습니다."),
+    INVALID_IMAGE_TYPE(HttpStatus.INTERNAL_SERVER_ERROR, "U010", "지원되지 않는 이미지 형식입니다. (JPEG, PNG, WebP만 허용"),
+    INVALID_FILE_NAME(HttpStatus.INTERNAL_SERVER_ERROR, "U011", "유효하지 않은 파일명입니다."),
+
 
     // 닉네임 관련 에러
     NICKNAME_WORDS_NOT_LOADED(HttpStatus.INTERNAL_SERVER_ERROR, "N001", "닉네임 생성을 위한 단어가 로드되지 않았습니다."),
@@ -55,7 +61,6 @@ public enum ErrorCode {
     NICKNAME_RESOURCE_LOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "N004", "닉네임 리소스 파일 로드에 실패했습니다."),
     NICKNAME_GENERATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "N005", "닉네임 생성에 실패했습니다."),
     NICKNAME_ALL_COMBINATIONS_EXHAUSTED(HttpStatus.SERVICE_UNAVAILABLE, "N006", "사용 가능한 모든 닉네임 조합이 소진되었습니다."),
-
 
     // 매칭 관련 에러
     ALREADY_IN_QUEUE(HttpStatus.CONFLICT, "M001", "이미 매칭 대기열에 참가하고 있습니다."),
