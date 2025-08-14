9a399dba15dc045f7a531cf40397a8b80c4bdf59
nan0silver <silvernh1220@gmail.com>
2025-08-14 23:57:38 +0900

    feat: 유효성 검증을 위해 @Validated 추가

--- src/main/java/com/ldsilver/chingoohaja/config/FirebaseProperties.java
+++ src/main/java/com/ldsilver/chingoohaja/config/FirebaseProperties.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
 @Component
+@Validated
 @ConfigurationProperties(prefix = "app.firebase")
 public class FirebaseProperties {
 
