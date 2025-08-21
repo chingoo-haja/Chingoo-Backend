e2dacf12279dd1bd38673f0531ed7f9957b96c6d
nan0silver <silvernh1220@gmail.com>
2025-08-21 14:55:51 +0900

    refactor: categoryType을 string -> enum으로 수정

--- src/main/java/com/ldsilver/chingoohaja/dto/category/response/CategoryResponse.java
+++ src/main/java/com/ldsilver/chingoohaja/dto/category/response/CategoryResponse.java
@@ -2,6 +2,7 @@ package com.ldsilver.chingoohaja.dto.category.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ldsilver.chingoohaja.domain.category.Category;
+import com.ldsilver.chingoohaja.domain.category.enums.CategoryType;
 
 import java.time.LocalDateTime;
 
@@ -9,7 +10,7 @@ public record CategoryResponse(
         @JsonProperty("id") Long id,
         @JsonProperty("name") String name,
         @JsonProperty("is_active") boolean isActive,
-        @JsonProperty("category_type") String categoryType,
+        @JsonProperty("category_type") CategoryType categoryType,
         @JsonProperty("created_at") LocalDateTime createdAt,
         @JsonProperty("updated_at") LocalDateTime updatedAt
 ) {
@@ -18,7 +19,7 @@ public record CategoryResponse(
                 category.getId(),
                 category.getName(),
                 category.isActive(),
-                category.getCategoryType().name(),
+                category.getCategoryType(),
                 category.getCreatedAt(),
                 category.getUpdatedAt()
         );
