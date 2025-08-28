e404388460aa5c97142d7e9fe2576afcb03e9554
nan0silver <silvernh1220@gmail.com>
2025-08-28 17:49:18 +0900

    style: 오타 수정'

--- src/main/java/com/ldsilver/chingoohaja/common/exception/ErrorCode.java
+++ src/main/java/com/ldsilver/chingoohaja/common/exception/ErrorCode.java
@@ -21,7 +21,7 @@ public enum ErrorCode {
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A005", "유효하지 않은 리프레시 토큰입니다."),
     IS_NOT_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A006", "리프레시 토큰이 아닙니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "A007", "데이터베이스에서 리프레시 토큰을 찾을 수 없습니다."),
-    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "A008", "유효하지 않은 엑세스 토큰입니다."),
+    INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "A008", "유효하지 않은 액세스 토큰입니다."),
 
     // JWT 관련 에러
     JWT_INVALID(HttpStatus.UNAUTHORIZED, "J001", "유효하지 않은 JWT 토큰입니다."),
