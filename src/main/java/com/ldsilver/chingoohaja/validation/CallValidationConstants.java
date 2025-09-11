b7f99abaa3ee62f478d6507716fb17c6203b1145
nan0silver <silvernh1220@gmail.com>
2025-09-11 13:46:05 +0900

    fix: 클래스명 오타 수정

--- src/main/java/com/ldsilver/chingoohaja/validation/CallValidationConstants.java
+++ src/main/java/com/ldsilver/chingoohaja/validation/CallValidationConstants.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 import java.util.regex.Pattern;
 
 @UtilityClass
-public class CallValicationConstants {
+public class CallValidationConstants {
 
     public static final Pattern CHANNEL_NAME_PATTERN = Pattern.compile("^[\\p{ASCII}&&[^\\p{Cntrl}]]+$"); // 또는 SDK 허용 문자 리스트로 구성
 
