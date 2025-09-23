a6f228306e42a2193b68902369a2418741218c73
nan0silver <silvernh1220@gmail.com>
2025-09-24 00:46:34 +0900

    feat: /reinitialize 관리자 권한으로 보호

--- src/main/java/com/ldsilver/chingoohaja/controller/AgoraHealthController.java
+++ src/main/java/com/ldsilver/chingoohaja/controller/AgoraHealthController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,6 +45,7 @@ public class AgoraHealthController {
                     "연결 문제가 발생했을 때 관리자가 수동으로 재시도할 수 있습니다."
     )
     @GetMapping("/reinitialize")
+    @PreAuthorize("hasRole('ADMIN')")
     public ApiResponse<AgoraHealthStatus> reinitializeAgora() {
         log.info("Agora 서비스 재초기화 요청");
 
