43f408d00bb7b63dca76902a18754cbe69a3cd2a
nan0silver <silvernh1220@gmail.com>
2025-10-13 16:24:06 +0900

    feat: redis config파일 cluster관련 코드 정리

--- src/main/java/com/ldsilver/chingoohaja/config/RedisConfig.java
+++ src/main/java/com/ldsilver/chingoohaja/config/RedisConfig.java
@@ -1,9 +1,13 @@
 package com.ldsilver.chingoohaja.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
@@ -12,17 +16,47 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password:}")
+    private String password;
+
+    @Value("${spring.data.redis.ssl.enabled:false}")
+    private boolean sslEnabled;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        // Standalone 설정만 사용
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+
+        if (password != null && !password.isEmpty()) {
+            config.setPassword(password);
+        }
+
+        LettuceClientConfiguration.LettuceClientConfigurationBuilder clientConfigBuilder =
+                LettuceClientConfiguration.builder();
+
+        if (sslEnabled) {
+            clientConfigBuilder.useSsl();
+        }
+
+        return new LettuceConnectionFactory(config, clientConfigBuilder.build());
+    }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
         StringRedisSerializer stringSerializer = new StringRedisSerializer();
-        // Key Serializer
         template.setKeySerializer(stringSerializer);
         template.setHashKeySerializer(stringSerializer);
-
-        // Value Serializer
         template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
         template.setHashValueSerializer(new GenericToStringSerializer<>(Object.class));
 
