f9093f12cf16d3bd96f201315a35831a14354cc5
nan0silver <silvernh1220@gmail.com>
2025-11-12 14:05:31 +0900

    fix: 비밀번호 패턴 검증 삭제 - SignUpRequest

--- src/main/java/com/ldsilver/chingoohaja/dto/auth/request/SignUpRequest.java
+++ src/main/java/com/ldsilver/chingoohaja/dto/auth/request/SignUpRequest.java
@@ -19,8 +19,6 @@ public record SignUpRequest(
 
         @NotBlank(message = "비밀번호는 필수입니다.")
         @Size(min = 8, max = 20, message = "비밀번호는 8-20자여야 합니다.")
-        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]+$",
-                message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
         @JsonProperty("password")
         String password,
 
