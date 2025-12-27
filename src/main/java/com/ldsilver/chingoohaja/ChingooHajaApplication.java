56fef3630a278c32028a5e32ff9ed9852484fdc2
nan0silver <silvernh1220@gmail.com>
2025-12-27 18:43:26 +0900

    fix: 운영DB 시간대 서울로 수정

--- src/main/java/com/ldsilver/chingoohaja/ChingooHajaApplication.java
+++ src/main/java/com/ldsilver/chingoohaja/ChingooHajaApplication.java
@@ -1,12 +1,21 @@
 package com.ldsilver.chingoohaja;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class ChingooHajaApplication {
+
+	@PostConstruct
+	public void init() {
+		// 애플리케이션 전체 타임존을 한국 시간으로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChingooHajaApplication.class, args);
